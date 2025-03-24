88a284a00f02e73370596789a59bcb9efe6e485d
Guillem <guillem@disedit.com>
2025-03-24 20:27:58 +0100

    add Image component

--- storyblok/Card.vue
+++ storyblok/Card.vue
@@ -3,7 +3,7 @@ defineProps({ blok: Object })
 </script>
 
 <template>
-  <div v-editable="blok" class="bg-white border-3 p-site">
+  <div v-editable="blok" class="bg-white border-3 p-site flex flex-col gap-site justify-between">
     <StoryblokComponent
       v-for="component in blok.blocks"
       :key="component._uid"
---
+++ storyblok/Image.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<template>
+  <img
+    :src="blok.image?.filename"
+    :alt="blok.image?.alt"
+    class="w-full block"
+  />
+</template>

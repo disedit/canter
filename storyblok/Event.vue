d1d9989be6466f8458ccb37b1f8400270b58ddb1
Guillem <guillem@disedit.com>
2025-04-17 12:32:52 +0200

    adapt events for mobile

--- storyblok/Event.vue
+++ storyblok/Event.vue
@@ -3,19 +3,19 @@ defineProps({ blok: Object })
 </script>
 
 <template>
-  <li v-editable="blok" class="border-b-3 border-black py-4 -tracking-widest">
-    <div class="uppercase text-lg flex gap-4 items-baseline">
+  <li v-editable="blok" class="border-b-3 border-black py-4 -tracking-widest flex flex-col gap-4">
+    <div class="uppercase text-lg flex gap-4 items-baseline leading-[1]">
       <IconArrow class="h-[1em] relative top-[.1em]"/>
       <span>
         {{ blok.date }}
         {{ blok.location }}
       </span>
-      <span class="text-md">
+      <span class="text-md text-right md:text-left ms-auto md:ms-0">
         {{ blok.venue }}
         {{ blok.time }}
       </span>
     </div>
-    <h2 class="uppercase font-bold text-lg text-right">
+    <h2 class="uppercase font-bold text-lg md:text-right leading-[1]">
       {{ blok.artists }}
     </h2>
   </li>

3f0f0e4791a9841a6ee000042fe95563390805c3
Guillem <guillem@disedit.com>
2025-04-17 12:28:28 +0200

    add events

---
+++ storyblok/Event.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<template>
+  <li v-editable="blok" class="border-b-3 border-black py-4 -tracking-widest">
+    <div class="uppercase text-lg flex gap-4 items-baseline">
+      <IconArrow class="h-[1em] relative top-[.1em]"/>
+      <span>
+        {{ blok.date }}
+        {{ blok.location }}
+      </span>
+      <span class="text-md">
+        {{ blok.venue }}
+        {{ blok.time }}
+      </span>
+    </div>
+    <h2 class="uppercase font-bold text-lg text-right">
+      {{ blok.artists }}
+    </h2>
+  </li>
+</template>
---
+++ storyblok/Events.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<template>
+  <div v-editable="blok">
+    <div class="text-md">
+      <UtilsRichText :content="blok.content" />
+    </div>
+    <ul class="border-t-3">
+      <StoryblokComponent
+        v-for="component in blok.events"
+        :key="component._uid"
+        :blok="component"
+      />
+    </ul>
+    
+  </div>
+</template>

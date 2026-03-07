ad4e0cb133ff5bb1f6b0050b871073727cb7955d
Guillem <guillem@disedit.com>
2026-03-07 18:35:46 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2024-11-02',
   ssr: process.env.NUXT_PUBLIC_NODE_ENV === 'production' ? true : false,
   runtimeConfig: {
     public: {
